4d92d082a246d017375d8247c5f1d89a990f5b48
Michael Crump <mbcrump29@gmail.com>
2016-02-23 17:07:41 -0800

    first commit

--- WindowsBridgeTestTemplate/ViewController.m
+++ WindowsBridgeTestTemplate/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)ClickMeButton:(id)sender {
+    NSLog(@"Clicked");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
